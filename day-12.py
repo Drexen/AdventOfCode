51272160383aa531b0c5b4fcb4327fe41ddb3f7e
Andrew Warrender <andrewwarrender@hotmail.co.uk>
2019-12-12 18:44:30 +0000

    Day 12 part two

--- day-12.py
+++ day-12.py
@@ -1,13 +1,14 @@
 import re
+import copy
 
 
 def solve():
     partOne()
+    partTwo()
 
 
 def partOne():
     positions = getData()
-    # velocities = [[0]*3]*len(positions)
     velocities = [[0,0,0], [0,0,0], [0,0,0], [0,0,0]]
     num_moons = len(positions)
 
@@ -40,6 +41,60 @@ def partOne():
         total_energy += (potential * kinetic)
 
     print("PartOne - Total system energy = " + str(total_energy))
+
+
+def partTwo():
+    x = getItersToMatchOriginalsOnAxis(0)
+    y = getItersToMatchOriginalsOnAxis(1)
+    z = getItersToMatchOriginalsOnAxis(2)
+
+    a = x * y / gcd(x, y)
+    b = a * z / gcd(a, z)
+
+    print("PartTwo: Num iters to original = " + str(int(b)))
+
+
+def gcd(a, b):
+    while b > 0:
+        temp = b
+        b = a % b
+        a = temp
+
+    return a
+
+
+def getItersToMatchOriginalsOnAxis(axis):
+    positions = getData()
+    velocities = [[0,0,0], [0,0,0], [0,0,0], [0,0,0]]
+    num_moons = len(positions)
+
+    positions_original = copy.deepcopy(positions)
+    velocities_original = copy.deepcopy(velocities)
+
+    iter = 0
+    while True:
+        iter += 1
+        # apply gravity
+        for a in range(0, num_moons):
+            for b in range(a + 1, num_moons):
+                if a != b:
+                    i = axis
+                    if positions[a][i] > positions[b][i]:
+                        velocities[a][i] -= 1
+                        velocities[b][i] += 1
+                    elif positions[a][i] < positions[b][i]:
+                        velocities[a][i] += 1
+                        velocities[b][i] -= 1
+
+        # apply velocity
+        for a in range(0, num_moons):
+            i = axis
+            positions[a][i] = positions[a][i] + velocities[a][i]
+
+        if positions == positions_original and velocities == velocities_original:
+            break
+
+    return iter
 
 
 def getData():
